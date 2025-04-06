0f5137358e33d72ae51ea893c25572373b43690e
София Кузнецова <skworkacc@outline.com>
2025-04-06 14:49:10 +0300

    реализовала шифрование простой замены, добавила базовую настройку CMakeLists и build, добавила globals

---
+++ build/CMakeFiles/encoder_AaP.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encoder_AaP.dir/main.cpp.obj"
+  "CMakeFiles/encoder_AaP.dir/main.cpp.obj.d"
+  "CMakeFiles/encoder_AaP.dir/work_folder/globals.cpp.obj"
+  "CMakeFiles/encoder_AaP.dir/work_folder/globals.cpp.obj.d"
+  "CMakeFiles/encoder_AaP.dir/work_folder/substitution_ciphers/caesar/CaesarCipher.cpp.obj"
+  "CMakeFiles/encoder_AaP.dir/work_folder/substitution_ciphers/caesar/CaesarCipher.cpp.obj.d"
+  "encoder_AaP.exe"
+  "encoder_AaP.exe.manifest"
+  "encoder_AaP.pdb"
+  "libencoder_AaP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/encoder_AaP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
-
+#include "work_folder/substitution_ciphers/caesar/CaesarCipher.hpp"
+#include <cctype>
 
 int main()
 {
+    CaesarCipher cipher;
+    std::string text = "HELLO WORLD";
+    std::string key = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+
+    std::cout << cipher.caesar_encryption(text, key) << std::endl;
+
+    std::string cipher_text = "SVOOL DLIOW";
+    std::cout << cipher.caesar_decryption(cipher_text, key) << std::endl;
+
     return 0;
 }
