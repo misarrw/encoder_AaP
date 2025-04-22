a066b697ca96effb43b355e185c8810500d5e30a
София Кузнецова <skworkacc@outline.com>
2025-04-22 23:15:31 +0300

    виженер: ключ по открытому тексту

---
+++ work_folder/gamma_ciphers/vigenere_cipher/gamma_generation/opentext_gamma/OpentextGamma.h
@@ -0,0 +1,10 @@
+# include "../../VigenereCipher.h"
+# include <vector>
+# include <string>
+
+
+class OpentextGamma : public VigenereCipher
+{
+public: 
+    std::vector<int> opentext(const std::string& key, std::vector<int>& text_numbers, int option);
+};
